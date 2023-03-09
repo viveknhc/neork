35ea8d6021eafb0de046b419d07d729170d0b49c
viveknhc <viveknhc@gmail.com>
2023-03-09 14:33:53 -0800

    hii

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,21 @@ $(document).ready(function () {
         loop: true,
         margin: 10,
         nav: true,
+        navText:[
+            '<span id="right-arrow" class="arrow right"><i class="fa fa-thin fa-arrow-left fa-2x"></i></span>',
+            '<span id="left-arrow" class="arrow left "> <i class="fa fa-thin fa-arrow-right fa-2x"></i></span>'
+        ],
         responsive: {
             0: {
                 items: 1
             },
             600: {
-                items: 1
+                items: 1,
+                nav: false,
             },
             1000: {
-                items: 1
+                items: 1,
+                nav: false,
             }
         }
     });
@@ -28,15 +34,21 @@ $(document).ready(function () {
 $(document).ready(function () {
     $("#testimonial").owlCarousel({
         loop: true,
-        autoplay:true,
+        // autoplay:true,
         margin: 10,
         nav: true,
+        navText: [
+            '<i class="fa fa-thin fa-arrow-left fa-2x"></i>',
+            '<i class="fa fa-thin fa-arrow-right fa-2x"></i>'
+        ],
         responsive: {
             0: {
-                items: 1
+                items: 1,
+                nav: false,
             },
             600: {
-                items: 2
+                items: 2,
+                nav: false,
             },
             1000: {
                 items: 2
@@ -44,4 +56,5 @@ $(document).ready(function () {
         }
     });
 });
+
 
